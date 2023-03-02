95fecec634bec61da3bd6e94eef9839f26c8efd0
Mikhail Biketov <mihhha985@yandex.ru>
2023-03-02 10:00:29 +0300

    fixed mistake from stepWrapper component

--- client/components/StepWrapper.tsx
+++ client/components/StepWrapper.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 interface StepWrapperProps{
     activeStep: number;
+    children:any;
 }
 
-const StepWrapper: React.FC<StepWrapperProps> = ({children}) => {
+const StepWrapper: React.FC<StepWrapperProps> = ({activeStep = 0, children}) => {
 
     return (
         <div>
+            {activeStep === 0 && 'done!'}
             {children}    
         </div>
     )
